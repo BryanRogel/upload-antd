9c59f07445b9e215fcf977bb7f3dbfb3af5d4db1
BryanRogel <bryanrogeldev@gmail.com>
2025-07-11 09:52:27 -0600

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,99 +1,46 @@
 <template>
-  <div>
-    <template v-if="props.title">
-      <h4 style="color: var(--black) !important;">{{ props.title }}</h4>
-    </template>
-    <slot v-else name="title" />
-    <template v-if="props.description">
-      <div class="mb-3">{{ props.description }}</div>
-    </template>
-    <slot v-else name="description" />
+  <a-upload
+    v-model:file-list="fileList"
+    name="file"
+    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+    :headers="headers"
+    @change="handleChange"
+    :capture="null"
 
-    <a-upload
-      list-type="picture"
-      :file-list="files"
-      :multiple="props.multiple"
-      :before-upload="() => false"
-      @remove="handleRemove"
-    >
-      <a-button class="mt-2" @click.prevent="triggerInput">
-        <upload-outlined />
-        Subir Archivos
-      </a-button>
-    </a-upload>
-
-    <input
-      ref="fileInput"
-      type="file"
-      :multiple="props.multiple"
-      :accept="accept"
-      style="display: none"
-      @change="handleManualUpload"
-    />
-  </div>
+  >
+    <a-button>
+      <upload-outlined></upload-outlined>
+      Click to Upload
+    </a-button>
+  </a-upload>
 </template>
-
-<script setup>
-import { UploadOutlined } from '@ant-design/icons-vue'
-import { defineProps, defineEmits, ref, watch, onUnmounted } from 'vue'
-
-const props = defineProps({
-  files: {
-    type: Array,
-    required: true,
-    default: () => []
+<script>
+import { message } from 'ant-design-vue';
+import { UploadOutlined } from '@ant-design/icons-vue';
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  components: {
+    UploadOutlined,
   },
-  title: {
-    type: String,
-    default: null
+  setup() {
+    const handleChange = info => {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    };
+    const fileList = ref([]);
+    return {
+      fileList,
+      headers: {
+        authorization: 'authorization-text',
+      },
+      handleChange,
+    };
   },
-  description: {
-    type: String,
-    default: null
-  },
-  multiple: {
-    type: Boolean,
-    default: true
-  }
-})
-
-const emit = defineEmits(['update:files'])
-const files = ref([])
-const fileInput = ref(null)
-
-const accept = [
-  'image/*',
-  'application/pdf',
-  'application/msword',
-  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-  'text/plain'
-].join(',')
-
-const triggerInput = () => {
-  fileInput.value?.click()
-}
-
-const handleManualUpload = (e) => {
-  const selected = Array.from(e.target.files).map((file, index) => ({
-    uid: `${Date.now()}-${index}`,
-    name: file.name,
-    status: 'done',
-    url: URL.createObjectURL(file)
-  }))
-  files.value = props.multiple ? [...files.value, ...selected] : [selected[0]]
-  emit('update:files', files.value)
-}
-
-const handleRemove = file => {
-  files.value = files.value.filter(f => f.uid !== file.uid)
-  emit('update:files', files.value)
-}
-
-watch(() => props.files, (val) => {
-  files.value = val
-}, { immediate: true })
-
-onUnmounted(() => {
-  files.value = []
-})
+});
 </script>
