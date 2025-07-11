6a1bf1c6577ec6f2513a1dba0cd3287dc748bc1c
BryanRogel <bryanrogeldev@gmail.com>
2025-07-11 09:02:21 -0600

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,64 +1,99 @@
 <template>
-  <a-upload
-    v-model:file-list="fileList"
-    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-    :before-upload="beforeUpload"
-    @change="handleChange"
-  >
-    <a-button>
-      <upload-outlined></upload-outlined>
-      Upload png only
+  <div>
+    <h1>xd</h1>
+    <a-upload
+      v-model:file-list="fileList"
+      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+      :before-upload="beforeUpload"
+      @change="handleChange"
+      :multiple="true"
+      :show-upload-list="true"
+      :accept="acceptedTypes"
+    >
+      <a-button>
+        <upload-outlined />
+        Subir archivos (imágenes, PDF, DOC, etc.)
+      </a-button>
+    </a-upload>
+    
+    <!-- input "manual" para máxima compatibilidad (opcional extra) -->
+    <input
+      ref="rawInput"
+      type="file"
+      :accept="acceptedTypes"
+      multiple
+      style="display: none"
+      @change="handleManualFile"
+    />
+    <a-button class="mt-2" @click="triggerRawInput">
+      📁 Elegir archivo (máxima compatibilidad)
     </a-button>
-  </a-upload>
+  </div>
 </template>
+
 <script>
 import { UploadOutlined } from '@ant-design/icons-vue';
 import { defineComponent, ref } from 'vue';
 import { message, Upload } from 'ant-design-vue';
+
 export default defineComponent({
   components: {
     UploadOutlined,
   },
   setup() {
-    const fileList = ref([{
-      uid: '1',
-      name: 'xxx.png',
-      status: 'done',
-      response: 'Server Error 500',
-      // custom error message to show
-      url: 'http://www.baidu.com/xxx.png',
-    }, {
-      uid: '2',
-      name: 'yyy.png',
-      status: 'done',
-      url: 'http://www.baidu.com/yyy.png',
-    }, {
-      uid: '3',
-      name: 'zzz.png',
-      status: 'error',
-      response: 'Server Error 500',
-      // custom error message to show
-      url: 'http://www.baidu.com/zzz.png',
-    }]);
-    const handleChange = ({
-      file,
-      fileList,
-    }) => {
-      if (file.status !== 'uploading') {
-        console.log(file, fileList);
-      }
+    const fileList = ref([]);
+    const rawInput = ref(null);
+
+    const acceptedTypes = [
+      'image/png',
+      'image/jpeg',
+      'image/jpg',
+      'application/pdf',
+      'application/msword',
+      'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      'text/plain',
+    ].join(',');
+
+    const handleChange = ({ file, fileList: newList }) => {
+       console.log('Archivo actual:', file);
+      console.log('Archivos cargados:', newList);
+      fileList.value = newList;
     };
-    const beforeUpload = file => {
-      const isPNG = file.type === 'image/png';
-      if (!isPNG) {
-        message.error(`${file.name} is not a png file`);
+
+    const beforeUpload = (file) => {
+      const isAccepted = acceptedTypes.includes(file.type);
+      if (!isAccepted) {
+        message.error(`${file.name} no es un tipo de archivo permitido.`);
       }
-      return isPNG || Upload.LIST_IGNORE;
+      return isAccepted || Upload.LIST_IGNORE;
     };
+
+    const triggerRawInput = () => {
+      rawInput.value?.click();
+    };
+
+    const handleManualFile = (event) => {
+      const files = Array.from(event.target.files);
+      files.forEach((file, index) => {
+        console.log('Archivo seleccionado manualmente:', file);
+        // Podrías subir manualmente aquí o hacer lo que necesites con ellos
+        fileList.value.push({
+          uid: `${Date.now()}-${index}`,
+          name: file.name,
+          status: 'done',
+          url: URL.createObjectURL(file),
+        });
+      });
+    };
+
     return {
       fileList,
       handleChange,
       beforeUpload,
+      acceptedTypes,
+      rawInput,
+      triggerRawInput,
+      handleManualFile,
     };
   },
 });
